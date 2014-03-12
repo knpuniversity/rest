f2bab2752da1e4f49a41b32988349687b27f560e
Ryan Weaver <ryan@thatsquality.com>
2014-03-12 18:26:15 -0500

    Tweaking example route

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -10,7 +10,7 @@ class ProgrammerController extends BaseController
 {
     protected function addRoutes(ControllerCollection $controllers)
     {
-        //$controllers->get('/programmers/new', array($this, 'newAction'))->bind('programmer_new');
+        // $controllers->post('/api/programmers', array($this, 'newAction'));
     }
 
-} 
+}
