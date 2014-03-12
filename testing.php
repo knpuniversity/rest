8c8c246eba81cb53895d76230391cdf76b2e7769
Ryan Weaver <ryan@thatsquality.com>
2014-03-12 18:30:04 -0500

    Moving Guzzle to the right spot in composer and adding a little, ugly test script

---
+++ testing.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use Guzzle\Http\Client;
+
+// create our http client (Guzzle)
+$client = new Client('http://localhost:8000', array(
+    'request.options' => array(
+        'exceptions' => false,
+    )
+));
+
