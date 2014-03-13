6f918c02fa72960901eb7f0f32d3aa4653df660f
Leanna Pelham <leanna.pelham@knplabs.com>
2014-03-13 17:36:50 -0500

    lots of design updates to the battle app

--- src/KnpU/CodeBattle/Battle/PowerManager.php
+++ src/KnpU/CodeBattle/Battle/PowerManager.php
@@ -13,16 +13,40 @@ class PowerManager
     private $programmerRepository;
 
     private static $positiveMessages = array(
-        'Wow, you just read the documentation from beginning to end! That\'s %s more energy for you.',
+        'Wow, you read the whole documentation! That\'s %s more energy for you.',
         'You just got back from SunshinePHP in sunny Miami. That\'s worth %s more energy!',
-        'When do you sleep!? You read RESTful web APIs cover to cover - %s energy for you',
-        'You went for a walk and the solution just came to you - %s more energy.'
+        'When do you sleep!? You read RESTful web APIs cover to cover: %s energy for you',
+        'You went for a walk and the solution just came to you: %s more energy.',
+		'You had 3 beers and hit your Ballmer Peak: %s more energy!',
+		'You went for a jog and thought of the answer! %s more energy',
+		'You remembered to feed your cat! %s energy',
+		'Congrats - you created the newest InstaFaceTweet, that\'s %s more energy!',
+		'Your investor check arrived %s energy',
+		'Your internet went down and productivity is way way up! %s energy',
+		'Attended a geek breakfast - food in your belly means %s energy!',
+		'Working from home = no meetings! %s energy',
+		'Free donuts in the breakroom: %s energy',
+		'Client check arrive in the mail. ChaChing %s',
+		'Built a custom standup desk: %s energies!',
+		'Your tests pass! %s energy',
+		'You make bootstrap look good, %s energy',
     );
 
     private static $negativeMessages = array(
-        'You *meant* to read something, but watched re-runs of Star Trek instead. Awesome, but that\'ll cost you %s energy',
-        'You fell asleep at the office while trying to read the docs and your co-workers think your kinda weird: %s energy',
+        'You *meant* to read something, but watched re-runs of Star Trek instead. Awesome, but that\'ll cost %s energy',
+        'You fell asleep at the office while reading the docs and your co-workers think you\'re kinda weird: %s energy',
         'Drank too much Red Bull and ran laps around the office instead of watching a screencast: %s energy',
+		'You drank past your Ballmer Peak and are now sleeping: %s energy',
+		'Bummer, you caught the flu %s energy',
+		'Your cat spilled coffee on your keyboard %s energy',
+		'Womp womp, you don\'t have a solid state harddrive that\'s %s energy.',
+		'A new wearable is available and productivity is down: %s energy.',
+		'Hand cramp! %s energy',
+		'You cut out early for happy hour: %s energy',
+		'Client asks you to make the logo bigger, %s energy',
+		'Your cat got caught in the blinds %s energy',
+		'The dinosaurs...they know how to open doors! %s energy',
+		
     );
 
     public function __construct(ProgrammerRepository $programmerRepository)
