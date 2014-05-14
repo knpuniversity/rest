4fc3dc7b78d5c6b6f99fd3b5f795c7e924312eee
Ryan Weaver <ryan@thatsquality.com>
2014-05-14 12:23:21 -0500

    Further clarifying the magic we're doing on our ApiEntryPoint

--- src/KnpU/CodeBattle/Security/Authentication/ApiEntryPoint.php
+++ src/KnpU/CodeBattle/Security/Authentication/ApiEntryPoint.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\InsufficientAuthenticationException;
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 
 /**
@@ -26,15 +27,32 @@ class ApiEntryPoint implements AuthenticationEntryPointInterface
      */
     public function start(Request $request, AuthenticationException $authException = null)
     {
-        if ($authException) {
-            $message = $authException->getMessageKey();
-        } else {
-            $message = 'Authentication Required';
-        }
+        $message = $this->getMessage($authException);
 
         $response = new JsonResponse(array('details' => $message), 401);
 
         return $response;
     }
 
+    /**
+     * If we have an AuthenticationException, use its getMessageKey().
+     *
+     * But it should really be run through a translator.
+     *
+     * Without a translator, InsufficientAuthenticationException is special
+     * because it is the exception that occurs when we're denied access,
+     * but we're not actually logged in. In this case, we want to simply
+     * say "Authentication Required", but the internal message is much
+     * uglier than this. So, if we see this exception, we replace the
+     * message. If we were using a translator, the ugly message could
+     * be replaced with the nice message in the translation dictionary.
+     */
+    private function getMessage(AuthenticationException $authException = null)
+    {
+        if ($authException && !$authException instanceof InsufficientAuthenticationException) {
+            return $authException->getMessageKey();
+        };
+
+        return 'Authentication Required';
+    }
 }
