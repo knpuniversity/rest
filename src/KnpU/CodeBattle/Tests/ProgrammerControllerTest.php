e941416ffc69c20d3d1d018460fb5be68e6278b8
Ryan Weaver <ryan@thatsquality.com>
2014-05-28 15:58:53 -0500

    [tuts-apply-diff] Applying existing diff file for step behat-fill-in-phpunit-test

--- src/KnpU/CodeBattle/Tests/ProgrammerControllerTest.php
+++ src/KnpU/CodeBattle/Tests/ProgrammerControllerTest.php
@@ -1,7 +1,27 @@
 <?php
 namespace KnpU\CodeBattle\Tests;
 
+use Guzzle\Http\Client;
+
 class ProgrammerControllerTest extends \PHPUnit_Framework_TestCase
 {
+    public function testPOST()
+    {
+        // create our http client (Guzzle)
+        $client = new Client('http://localhost:8000', array(
+            'request.options' => array(
+                'exceptions' => false,
+            )
+        ));
 
+        $nickname = 'ObjectOrienter'.rand(0, 999);
+        $data = array(
+            'nickname' => $nickname,
+            'avatarNumber' => 5,
+            'tagLine' => 'a test dev!'
+        );
+
+        $request = $client->post('/api/programmers', null, json_encode($data));
+        $response = $request->send();
+    }
 }
