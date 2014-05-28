abe7745de728dc38139805d7a7f1ffb126c74a6c
Ryan Weaver <ryan@thatsquality.com>
2014-05-28 16:14:59 -0500

    [tuts-apply-diff] Applying existing diff file for step auth-basic-new-token-implementation

--- src/KnpU/CodeBattle/Controller/Api/TokenController.php
+++ src/KnpU/CodeBattle/Controller/Api/TokenController.php
@@ -4,6 +4,8 @@ namespace KnpU\CodeBattle\Controller\Api;
 
 use KnpU\CodeBattle\Controller\BaseController;
 use Silex\ControllerCollection;
+use Symfony\Component\HttpFoundation\Request;
+use KnpU\CodeBattle\Security\Token\ApiToken;
 
 class TokenController extends BaseController
 {
@@ -12,8 +14,19 @@ class TokenController extends BaseController
         $controllers->post('/api/tokens', array($this, 'newAction'));
     }
 
-    public function newAction()
+    public function newAction(Request $request)
     {
-        return 'foo';
+        $username = $request->headers->get('PHP_AUTH_USER');
+
+        $user = $this->getUserRepository()->findUserByUsername($username);
+
+        $data = json_decode($request->getContent(), true);
+
+        $token = new ApiToken($user->id);
+        $token->notes = $data['notes'];
+
+        $this->getApiTokenRepository()->save($token);
+
+        return $this->createApiResponse($token, 201);
     }
 }
