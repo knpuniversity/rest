548f881f6589f5556e5db911fe8d97d734cf3073
Ryan Weaver <ryan@thatsquality.com>
2014-05-28 16:15:01 -0500

    [tuts-apply-diff] Applying existing diff file for step battle-post-code-implementation

--- src/KnpU/CodeBattle/Controller/Api/BattleController.php
+++ src/KnpU/CodeBattle/Controller/Api/BattleController.php
@@ -4,6 +4,7 @@ namespace KnpU\CodeBattle\Controller\Api;
 
 use KnpU\CodeBattle\Controller\BaseController;
 use Silex\ControllerCollection;
+use Symfony\Component\HttpFoundation\Request;
 
 class BattleController extends BaseController
 {
@@ -12,8 +13,20 @@ class BattleController extends BaseController
         $controllers->post('/api/battles', array($this, 'newAction'));
     }
 
-    public function newAction()
+    public function newAction(Request $request)
     {
-        return 'foo';
+        $this->enforceUserSecurity();
+
+        $data = $this->getJsonBody($request);
+
+        $programmer = $this->getProgrammerRepository()->find($data->get('programmerId'));
+        $project = $this->getProjectRepository()->find($data->get('projectId'));
+
+        $battle = $this->getBattleManager()->battle($programmer, $project);
+
+        $response = $this->createApiResponse($battle, 201);
+        $response->headers->set('Location', 'TODO');
+
+        return $response;
     }
 }
