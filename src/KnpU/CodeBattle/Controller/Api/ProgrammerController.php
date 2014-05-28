386352fd7342e8590bce21feea09f7cf13de8467
Ryan Weaver <ryan@thatsquality.com>
2014-05-28 15:59:03 -0500

    [tuts-apply-diff] Applying existing diff file for step delete-programmer-implement-the-controller

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -21,6 +21,7 @@ class ProgrammerController extends BaseController
 
         $controllers->get('/api/programmers', array($this, 'listAction'));
         $controllers->put('/api/programmers/{nickname}', array($this, 'updateAction'));
+        $controllers->delete('/api/programmers/{nickname}', array($this, 'deleteAction'));
     }
 
     public function newAction(Request $request)
@@ -84,6 +85,17 @@ class ProgrammerController extends BaseController
         $response = new JsonResponse($data, 200);
 
         return $response;
+    }
+
+    public function deleteAction($nickname)
+    {
+        $programmer = $this->getProgrammerRepository()->findOneByNickname($nickname);
+
+        if ($programmer) {
+            $this->delete($programmer);
+        }
+
+        return new Response(null, 204);
     }
 
     /**
