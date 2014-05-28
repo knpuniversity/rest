c312dc70a74a059b178860bdc5122d818e5f98a4
Ryan Weaver <ryan@thatsquality.com>
2014-05-28 16:14:55 -0500

    [tuts-apply-diff] Applying existing diff file for step auth-requiring-a-valid-user-for-new-programmer

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ProgrammerController extends BaseController
 {
@@ -35,6 +36,10 @@ class ProgrammerController extends BaseController
 
     public function newAction(Request $request)
     {
+        if (!$this->isUserLoggedIn()) {
+            throw new AccessDeniedException();
+        }
+
         $programmer = new Programmer();
         $this->handleRequest($request, $programmer);
 
