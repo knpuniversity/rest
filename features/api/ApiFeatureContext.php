ea3c22bfeb5539dfe3980a36dd764d0b0c133553
Ryan Weaver <ryan@thatsquality.com>
2014-05-28 15:58:55 -0500

    [tuts-apply-diff] Applying existing diff file for step behat-clear-data-between-tests

--- features/api/ApiFeatureContext.php
+++ features/api/ApiFeatureContext.php
@@ -123,6 +123,14 @@ class ApiFeatureContext extends BehatContext
     }
 
     /**
+     * @BeforeScenario
+     */
+    public function clearData()
+    {
+        $this->getProjectHelper()->reloadDatabase();
+    }
+
+    /**
      * @Given /^I have the payload:$/
      */
     public function iHaveThePayload(PyStringNode $requestPayload)
