ac451dca642720c4320415239f1bc695175c6132
Ryan Weaver <ryan@thatsquality.com>
2014-05-30 16:24:11 -0500

    Moving programmer save into handleRequest, which I did in the video

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -27,7 +27,6 @@ class ProgrammerController extends BaseController
     {
         $programmer = new Programmer();
         $this->handleRequest($request, $programmer);
-        $this->save($programmer);
 
         $data = $this->serializeProgrammer($programmer);
         $response = new JsonResponse($data, 201);
@@ -77,7 +76,6 @@ class ProgrammerController extends BaseController
         }
 
         $this->handleRequest($request, $programmer);
-        $this->save($programmer);
 
         $data = $this->serializeProgrammer($programmer);
 
@@ -104,6 +102,8 @@ class ProgrammerController extends BaseController
         $programmer->avatarNumber = $data['avatarNumber'];
         $programmer->tagLine = $data['tagLine'];
         $programmer->userId = $this->findUserByUsername('weaverryan')->id;
+
+        $this->save($programmer);
     }
 
     private function serializeProgrammer(Programmer $programmer)
