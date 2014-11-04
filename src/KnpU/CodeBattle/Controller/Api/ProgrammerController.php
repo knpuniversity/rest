0721cc0e2be1d0f7666f97ae45fe3ae31c22d1d3
Ryan Weaver <ryan@thatsquality.com>
2014-11-03 20:11:22 -0500

    [tuts-apply-diff] Applying existing diff file for step get-programmer-return-body-on-post

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -32,12 +32,17 @@ class ProgrammerController extends BaseController
 
         $this->save($programmer);
 
-        $response = new Response('It worked. Believe me - I\'m an API', 201);
+        $data = $this->serializeProgrammer($programmer);
+        $response = new Response(
+            json_encode($data),
+            201
+        );
         $programmerUrl = $this->generateUrl(
             'api_programmers_show',
             ['nickname' => $programmer->nickname]
         );
         $response->headers->set('Location', $programmerUrl);
+        $response->headers->set('Content-Type', 'application/json');
 
         return $response;
     }
