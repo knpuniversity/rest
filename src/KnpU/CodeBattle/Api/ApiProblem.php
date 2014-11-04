06f86f7dd203ab16b4a24fcfae97a3a3c5bf52cc
Ryan Weaver <ryan@thatsquality.com>
2014-11-03 20:11:31 -0500

    [tuts-apply-diff] Applying existing diff file for step auth-apiproblem-on-error

--- src/KnpU/CodeBattle/Api/ApiProblem.php
+++ src/KnpU/CodeBattle/Api/ApiProblem.php
@@ -11,10 +11,12 @@ class ApiProblem
 {
     const TYPE_VALIDATION_ERROR = 'validation_error';
     const TYPE_INVALID_REQUEST_BODY_FORMAT = 'invalid_body_format';
+    const TYPE_AUTHENTICATION_ERROR = 'authentication_error';
 
     private static $titles = array(
         self::TYPE_VALIDATION_ERROR => 'There was a validation error',
         self::TYPE_INVALID_REQUEST_BODY_FORMAT => 'Invalid JSON format sent',
+        self::TYPE_AUTHENTICATION_ERROR => 'Invalid or missing authentication',
     );
 
     private $statusCode;
