77220a2eb651120e282b656e3fe9c3796afbcc9f
Ryan Weaver <ryan@thatsquality.com>
2014-11-03 20:11:32 -0500

    [tuts-apply-diff] Applying existing diff file for step auth-token-new-routing

--- src/KnpU/CodeBattle/Controller/Api/TokenController.php
+++ src/KnpU/CodeBattle/Controller/Api/TokenController.php
@@ -3,7 +3,17 @@
 namespace KnpU\CodeBattle\Controller\Api;
 
 use KnpU\CodeBattle\Controller\BaseController;
+use Silex\ControllerCollection;
 
 class TokenController extends BaseController
 {
+    protected function addRoutes(ControllerCollection $controllers)
+    {
+        $controllers->post('/api/tokens', array($this, 'newAction'));
+    }
+
+    public function newAction()
+    {
+        return 'foo';
+    }
 }
