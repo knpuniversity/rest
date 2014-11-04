33d5cf630efdfe9cf5bbcb69e30ef4f08a6d4cdd
Ryan Weaver <ryan@thatsquality.com>
2014-11-03 20:13:00 -0500

    [tuts-apply-diff] Applying existing diff file for step hateoas-linking-battle-to-programmer

--- src/KnpU/CodeBattle/Model/Battle.php
+++ src/KnpU/CodeBattle/Model/Battle.php
@@ -3,7 +3,17 @@
 namespace KnpU\CodeBattle\Model;
 
 use JMS\Serializer\Annotation as Serializer;
+use Hateoas\Configuration\Annotation as Hateoas;
 
+/**
+ * @Hateoas\Relation(
+ *      "programmer",
+ *      href = @Hateoas\Route(
+ *          "api_programmers_show",
+ *          parameters = { "nickname" = "expr(object.programmer.nickname)" }
+ *      )
+ * )
+ */
 class Battle
 {
     /* All public properties are persisted */
@@ -24,12 +34,4 @@ class Battle
     public $foughtAt;
 
     public $notes;
-
-    /**
-     * @Serializer\VirtualProperty()
-     */
-    public function getProgrammerUri()
-    {
-        return '/api/programmers/'.$this->programmer->nickname;
-    }
 }
