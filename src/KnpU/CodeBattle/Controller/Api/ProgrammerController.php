6f655b56a72f6aec743738f9177dcc903597debb
Ryan Weaver <ryan@thatsquality.com>
2014-11-06 18:25:54 -0500

    [tuts-apply-diff] Applying existing diff file for step patch-programmer-adding-the-controller

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -20,7 +20,13 @@ class ProgrammerController extends BaseController
             ->bind('api_programmers_show');
 
         $controllers->get('/api/programmers', array($this, 'listAction'));
+
+        // point PUT and PATCH at the same controller
         $controllers->put('/api/programmers/{nickname}', array($this, 'updateAction'));
+        // PATCH isn't natively supported, hence the different syntax
+        $controllers->match('/api/programmers/{nickname}', array($this, 'updateAction'))
+            ->method('PATCH');
+
         $controllers->delete('/api/programmers/{nickname}', array($this, 'deleteAction'));
     }
 
@@ -119,6 +125,11 @@ class ProgrammerController extends BaseController
 
         // update the properties
         foreach ($apiProperties as $property) {
+            // if a property is missing on PATCH, that's ok - just skip it
+            if (!isset($data[$property]) && $request->isMethod('PATCH')) {
+                continue;
+            }
+
             $val = isset($data[$property]) ? $data[$property] : null;
             $programmer->$property = $val;
         }
