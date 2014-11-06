e9c44fa008cc50c1ca59aed544881baec2551c1a
Ryan Weaver <ryan@thatsquality.com>
2014-11-06 18:25:56 -0500

    [tuts-apply-diff] Applying existing diff file for step errors-handling-404-s

--- src/KnpU/CodeBattle/Application.php
+++ src/KnpU/CodeBattle/Application.php
@@ -3,6 +3,7 @@
 namespace KnpU\CodeBattle;
 
 use Doctrine\Common\Annotations\AnnotationReader;
+use KnpU\CodeBattle\Api\ApiProblem;
 use KnpU\CodeBattle\Api\ApiProblemException;
 use KnpU\CodeBattle\Battle\PowerManager;
 use KnpU\CodeBattle\Repository\BattleRepository;
@@ -288,15 +289,25 @@ class Application extends SilexApplication
 
     private function configureListeners()
     {
-        $this->error(function(\Exception $e, $statusCode) {
-            // only do something special if we have an ApiProblemException!
-            if (!$e instanceof ApiProblemException) {
+        $app = $this;
+
+        $this->error(function(\Exception $e, $statusCode) use ($app) {
+            // only act on /api URLs
+            if (strpos($app['request']->getPathInfo(), '/api') !== 0) {
                 return;
             }
 
+            if ($e instanceof ApiProblemException) {
+                $apiProblem = $e->getApiProblem();
+            } else {
+                $apiProblem = new ApiProblem(
+                    $statusCode
+                );
+            }
+
             $response = new JsonResponse(
-                $e->getApiProblem()->toArray(),
-                $e->getApiProblem()->getStatusCode()
+                $apiProblem->toArray(),
+                $apiProblem->getStatusCode()
             );
             $response->headers->set('Content-Type', 'application/problem+json');
 
