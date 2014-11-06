5ae610416f7f0fd55aa5f8c561938ad609d60ac7
Ryan Weaver <ryan@thatsquality.com>
2014-11-06 18:25:56 -0500

    [tuts-apply-diff] Applying existing diff file for step errors-create-apiproblemexception

--- src/KnpU/CodeBattle/Api/ApiProblem.php
+++ src/KnpU/CodeBattle/Api/ApiProblem.php
@@ -8,9 +8,11 @@ namespace KnpU\CodeBattle\Api;
 class ApiProblem
 {
     const TYPE_VALIDATION_ERROR = 'validation_error';
+    const TYPE_INVALID_REQUEST_BODY_FORMAT = 'invalid_body_format';
 
     private static $titles = array(
-        self::TYPE_VALIDATION_ERROR => 'There was a validation error'
+        self::TYPE_VALIDATION_ERROR => 'There was a validation error',
+        self::TYPE_INVALID_REQUEST_BODY_FORMAT => 'Invalid JSON format sent',
     );
 
     private $statusCode;
@@ -53,5 +55,13 @@ class ApiProblem
     public function getStatusCode()
     {
         return $this->statusCode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
     }
 }
